edac85456bd829c2e91ffc8ea59f1f25c4ea21c1
fauadhaleem123 <fauad.haleem@devsinc.com>
2020-03-30 20:19:49 +0500

    heroku push

--- src/utils/api-config.js
+++ src/utils/api-config.js
@@ -3,9 +3,9 @@ var arr = hostname.split(".");
 const subDomain = arr[0];
 var url = "";
 if (arr[1] === "herokuapp") {
-  // topdomain = "backendherokuapp.com"
-  // url = hostname+topdomain
-  url = "https://devsinc-pos-backend.herokuapp.com";
+  // topdomain = "backendherokuapp.com" https://devsinc-pos-backend.herokuapp.com
+  // url = hostname+topdomain https://stormy-ocean-79147.herokuapp.com
+  url = "https://stormy-ocean-79147.herokuapp.com";
 } else if (
   (arr.length === 3 && arr[1] + "." + arr[2] === "lvh.me") ||
   (arr.length === 2 && arr[0] + "." + arr[1] === "lvh.me")
